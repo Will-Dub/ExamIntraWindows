57bc854a29e389f26477ec4ffee4e12289aa54e9
William Dubuc <william.dubuc01@gmail.com>
2023-11-10 10:21:23 -0500

    Ajout de la classe Electeur

---
+++ ExamWindows2/Classes/Electeur.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamWindows2.Classes
+{
+    internal class Electeur : Individu
+    {
+        // Attributs privés
+        private string numero_electeur;
+        private string circoncription;
+
+        // Propriétés
+        public string Numero_electeur
+        {
+            get { return this.numero_electeur; }
+            set { this.numero_electeur = value; }
+        }
+
+        public string Circoncription
+        {
+            get { return this.circoncription; }
+            set { this.circoncription = value; }
+        }
+
+        // Override functions
+        public override string ToString()
+        {
+            return "************\nNom: " + this.Nom + "\nPrenom: " + this.Prenom + "\nCourriel: " + this.Courriel + "\nNuméro de l'électeur: " + this.Numero_electeur +
+                "\nCirconscription: " + this.Circoncription + "\n************";
+        }
+
+
+        // Constructeur
+        Electeur(string p_nom, string p_prenom, string p_courriel, string p_numero_electeur, string p_circonscription) : base (p_nom, p_prenom, p_courriel)
+        {
+            this.numero_electeur = p_numero_electeur;
+            this.circoncription = p_circonscription;
+        }
+    }
+}
---
+++ ExamWindows2/Classes/Individu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamWindows2
+{
+    internal class Individu
+    {
+        // Attributs privés
+        private string nom;
+        private string prenom;
+        private string courriel;
+
+        // Propriétés
+        public string Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value; }
+        }
+
+        public string Prenom
+        {
+            get { return this.prenom; }
+            set { this.prenom = value; }
+        }
+        public string Courriel
+        {
+            get { return this.courriel; }
+            set { this.courriel = value; }
+        }
+
+        // Override functions
+        public override string ToString()
+        {
+            return "************\nNom: " + this.Nom + "\nPrenom: " + this.Prenom + "\nCourriel: " + this.Courriel + "\n************";
+        }
+
+        // Constructeur
+        public Individu(string p_nom, string p_prenom, string p_courriel)
+        {
+            this.nom = p_nom;
+            this.prenom = p_prenom;
+            this.prenom = p_courriel;
+        }
+
+    }
+}
