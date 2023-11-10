e4077ce1455fab415e924c498544f55cd7c6ba6e
William Dubuc <william.dubuc01@gmail.com>
2023-11-10 11:13:22 -0500

    Ajout du form FormElection

---
+++ ExamWindows2/Formulaire/FormElection.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ExamWindows2.Formulaire
+{
+    partial class FormElection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_title = new System.Windows.Forms.Label();
+            this.label_nom = new System.Windows.Forms.Label();
+            this.label_prenom = new System.Windows.Forms.Label();
+            this.button_creer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_title.ForeColor = System.Drawing.Color.Red;
+            this.label_title.Location = new System.Drawing.Point(244, 71);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(321, 37);
+            this.label_title.TabIndex = 0;
+            this.label_title.Text = "Gestion des élections";
+            // 
+            // label_nom
+            // 
+            this.label_nom.AutoSize = true;
+            this.label_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nom.Location = new System.Drawing.Point(183, 163);
+            this.label_nom.Name = "label_nom";
+            this.label_nom.Size = new System.Drawing.Size(51, 24);
+            this.label_nom.TabIndex = 1;
+            this.label_nom.Text = "Nom";
+            // 
+            // label_prenom
+            // 
+            this.label_prenom.AutoSize = true;
+            this.label_prenom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_prenom.Location = new System.Drawing.Point(183, 244);
+            this.label_prenom.Name = "label_prenom";
+            this.label_prenom.Size = new System.Drawing.Size(77, 24);
+            this.label_prenom.TabIndex = 2;
+            this.label_prenom.Text = "Prenom";
+            // 
+            // button_creer
+            // 
+            this.button_creer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_creer.Location = new System.Drawing.Point(338, 323);
+            this.button_creer.Name = "button_creer";
+            this.button_creer.Size = new System.Drawing.Size(126, 45);
+            this.button_creer.TabIndex = 3;
+            this.button_creer.Text = "Créer";
+            this.button_creer.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(359, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // FormElection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_creer);
+            this.Controls.Add(this.label_prenom);
+            this.Controls.Add(this.label_nom);
+            this.Controls.Add(this.label_title);
+            this.Name = "FormElection";
+            this.Text = "FormElection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.Label label_nom;
+        private System.Windows.Forms.Label label_prenom;
+        private System.Windows.Forms.Button button_creer;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
